cdd4d0a749e34db4e5c337d5627b274425df3558
Taylor Dugger <taylorsdugger@gmail.com>
2016-10-27 14:14:30 -0500

    a

--- Project2/project2.c
+++ Project2/project2.c
@@ -2,23 +2,37 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
+#include "Bank.h"
 
+#define bufferSize 64
+#define delim " \t\r\n\a"
 /*****************************************************************/
 int appserver(char **args);
+int check(char **args);
+int trans(char **args);
+int end(char **args);
+void *worker();
 
 /*****************************************************************/
 int reqID = 0;
 int numReq = 0;
-pthread_mutux_t mut;
-pthread_cond_t  worker_cv;
+pthread_mutex_t mut;
+pthread_cond_t  check_cv;
+pthread_cond_t  trans_cv;
+pthread_cond_t  end_cv;
 
 /*****************************************************************/
 struct account{
-    int ID = 0;
-    int bal = 0;
+    pthread_mutex_t lock;
+    int ID;
 };
 
-struct account accounts[numAccounts];
+struct request{
+    int value;
+
+};
+
+struct account accounts[0];
 
 /*****************************************************************/
 char *inputs[] = {
@@ -35,16 +49,28 @@ int (*functions[])(char **)={
         &end
 };
 
+//
+int numOfFunctions(){
+    return sizeof(inputs) / sizeof(char *);
+}
+
+//
 int appserver(char **args){
 
     int numThreads = args[0];
     int numAccounts = args[1];
     char *outFile = args[2];
 
-    pthread_mutux_init(&mut, NULL);
-    pthread_cond_init(&worker_cv, NULL);
+    int thread_index[numThreads];
+
+    pthread_cond_init(&check_cv, NULL);
+    pthread_cond_init(&end_cv, NULL);
+    pthread_cond_init(&trans_cv, NULL);
+
     pthread_t threads[numThreads];
 
+    struct account accounts[numAccounts];
+    initialize_accounts(numAccounts);
     for(int i = 0; i < numAccounts; i++){
         accounts[i].ID = i;
     }
@@ -62,27 +88,53 @@ int appserver(char **args){
 }
 
 //Worker threads
-void *worker(void *arg){
+void *worker(){
 
     pthread_mutux_lock(&mut);
     while(numReq == 0)
-        pthread_cond_wait(&threads, &mut);
+        pthread_cond_wait(&check_cv, &mut);
 
-    //go
+
+
+
+
+
+    //numReq--;
+    pthread_mutex_unlock(&mut);
 }
 
 //Balance check
 int check(char **args){
-    reqID++;
+    int i = 0;
+
     if(args[1] < 0){
         //throw
     }else{
-        int i = args[1];
+        i = args[1];
     }
 
-    struct account x = accounts[i];
+    pthread_mutex_lock(&mut);
+    //pthread_mutex_lock(&(accounts[i].lock));
+
+    reqID++;
+
+    pthread_cond_broadcast(&check_cv);
+
+    //struct account x = accounts[i];
 
     //printf(reqID + " BAL " + x.bal);
+
+    return 1;
+}
+
+int trans(char **args){
+
+    return 1;
+}
+
+int end(char **args){
+
+    return 1;
 }
 
 //ReadLine function
@@ -129,8 +181,8 @@ char **split(char *in){
 int execute(char **args){
     if(args[0] == NULL){return 1;}
 
-    for(int i = 0; i<4; i++){
-        if(strcmp(args[0], inputs[i]) == 0) return *functions[i](args);
+    for(int i = 0; i<numOfFunctions(); i++){
+        if(strcmp(args[0], inputs[i]) == 0) return (*functions[i])(args);
     }
 
     return start(args);
